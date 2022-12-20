027b707a790e924243778aa14ae736c9c22d9415
Ramya1520 <ramya.krishnan1520@gmail.com>
2022-12-20 16:52:59 +0530

    alternative method

--- src/component/textfeild.jsx
+++ src/component/textfeild.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useCallback} from "react";
+import React, { useState,useCallback,useRef} from "react";
 import './bookmark.css';
 import {
   Layout,
@@ -7,95 +7,71 @@ import {
   Button,
   FormLayout,
   TextField,
-  DataTable
+
+  
 } from "@shopify/polaris";
 
+var no = 1
 function BookmarkFrom() {
-
-  const [url, setUrl] = useState();
-  const [topic, setTopic] = useState();
-  const [notes, setNotes] = useState();
-  const [lists, setLists] =useState([]);
-
-  const handleChange = (newValue) => setUrl(newValue);
-  const handleChange1 = (newValue) => setTopic(newValue);
-  const handleChange2 =(newValue) => setNotes(newValue);
-
-  const Submit= useCallback(() => {
-
-   var details=[]
-      details.push(url)
-      details.push(topic)
-      details.push(notes)
-
-   if(url){setLists(() => [...lists, details])}
-   setUrl('')
-   setTopic('')
-   setNotes('')
    
-  }, [notes,url,topic]); 
-
-  return (
+    const [listVal, setListVal] = useState({
+      url:"",
+      topic:"",
+      notes:""
+    })
+    const [list, setList] = useState([{url:'',notes:'',value:''}])
     
-    <Page 
-      title="Bookmark"
-    >
-      <Layout>
-        <Card sectioned  >
-          <FormLayout>
-            <FormLayout.Group>
-          
+    const Submit = () => {
+        if (listVal.url && listVal.topic && listVal.notes) { setList([...list, listVal]) }
+        console.log("list=================>:", list)
+    }
 
-                <TextField className='.Polaris-TextField__Input'
-                    label="URL Link"
-                    value={url}
-                    onChange={handleChange}
-                    autoComplete="off"
-                    placeholder="paste or type your URL Link here"
-                />
-
-               <TextField
-                    label="Topic"
-                    value={topic}
-                    onChange={handleChange1}
-                    autoComplete="off"
-                    placeholder="Enter your topic here"
-                />
-
-            </FormLayout.Group>
-
-            <TextField
-                    label="Notes"
-                    value={notes}
-                    onChange={handleChange2}
-                    autoComplete="off"
-                    placeholder="Enter your notes here"
-                    multiline={4}
-                />
-            <Button primary onClick={Submit} >Submit</Button>
-          
-          </FormLayout>
-        </Card>
-      </Layout>
-
-      <p className="layout"></p>
-      <DataTable  className="e"
-          columnContentTypes={[
-            'text',
-            'text',
-            'text'
-          ]}
-          headings ={[
-           
-            'URL Link',
-            'Type',
-            'Notes',
-          ]}
-          rows={lists}          
-        />
-    </Page>
-    
-  )
-        }
    
+    return (
+        <Page
+        title="Bookmark"
+      >
+           <Layout>
+          <Card sectioned>
+            <FormLayout>
+              <FormLayout.Group>
+            
+                  <TextField
+                      label="URL Link"
+                      type="text"
+                      value={listVal.url}
+                      onChange={e=>setListVal({...listVal,url: e})}
+                      autoComplete="off"
+                      placeholder="paste or type your URL Link here"
+                  />
+  
+                 <TextField
+                      label="Topic"
+                      type="text"input
+                       value={listVal.topic}
+                       onChange={e=>setListVal({...listVal,topic: e})}
+                       autoComplete="off"
+                       placeholder="Enter your topic here"
+                  />
+              </FormLayout.Group>
+                <TextField
+                     label="Notes" 
+                      type="text"
+                      value={listVal.notes}
+                      onChange={e=>setListVal({...listVal,notes: e
+                      })}
+                      autoComplete="off"
+                      placeholder="Enter your notes here"
+                      multiline={4}
+                  />
+              <Button primary onClick={Submit} >Submit</Button>
+  
+            </FormLayout>
+          </Card>
+        </Layout>
+        <p className="layout"></p>
+      </Page> 
+    )   
+}
+
 export default BookmarkFrom
